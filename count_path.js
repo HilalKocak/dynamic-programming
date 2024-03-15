18d4ddce217605cc396e5e7964c61d65574000d0
Hilal38 <hhilalkocak@gmail.com>
2024-03-15 23:24:40 +0300

    Add max-path sum fn

--- count_path.js
+++ count_path.js
@@ -1,6 +1,7 @@
 /**
  count paths
 Write a function, countPaths, that takes in a grid as an argument. In the grid, 'X' represents walls and 'O' represents open spaces. You may only move down or to the right and cannot pass through walls. The function should return the number of ways possible to travel from the top-left corner of the grid to the bottom-right corner.
+O of pow(r*c)
  */
 
 const countPaths = (grid, r=0, c=0, memo={}) => {
